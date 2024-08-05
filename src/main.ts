99cbd94335280cba0c5df1b5e97722e4c8ffbc0b
Julio Cesar <cesar.lopez@t1paginas.com>
2024-08-05 10:31:04 -0600

    delete certs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // Configurar CORS
   app.use(
     cors({
-      origin: 'http://formulario-nextjs-nine.vercel.app/', // Permitir solicitudes desde este origen
+      origin: 'https://formulario-nextjs-nine.vercel.app/', // Permitir solicitudes desde este origen
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true, // Permitir el envío de cookies
     }),
