8d92578ecd9f72d83e4b87e6f985d7540392eb2d
Julio Cesar <cesar.lopez@t1paginas.com>
2024-08-05 11:49:29 -0600

    swagger Document

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Form API')
+    .setDescription(
+      'APi function to mongose and nextjs, only to post, get and delete',
+    )
+    .setVersion('1.0')
+    .addTag('Form')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Configurar CORS
   app.use(
