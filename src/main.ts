3ee4ec0b58ad2940a07f008df7eaddeaa57f3f19
Julio Cesar <cesar.lopez@t1paginas.com>
2024-08-05 10:13:12 -0600

    new config

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  let httpsOptions;
-  try {
-    httpsOptions = {
-      key: fs.readFileSync('./src/cert/key.pem'),
-      cert: fs.readFileSync('./src/cert/cert.pem'),
-    };
-  } catch (error) {
-    console.error('Error reading SSL certificate files:', error);
-    process.exit(1);
-  }
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   // Configurar CORS
   app.use(
     cors({
-      origin: 'https://main.d2le7ptoltic3y.amplifyapp.com', // Permitir solicitudes desde este origen
+      origin: 'http://formulario-nextjs-nine.vercel.app/', // Permitir solicitudes desde este origen
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true, // Permitir el envío de cookies
     }),
